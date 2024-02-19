4768102d0ca58e07c10124784f8ff1873458c20e
Fullstack selenium <dangtuananh94.it@gmail.com>
2024-02-19 15:50:39 +0700

    Fixed_Popup

--- selenium/webdriver/Topic_25_Fixed_Popup.java
+++ selenium/webdriver/Topic_25_Fixed_Popup.java
@@ -11,7 +11,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_25_Popup_I {
+public class Topic_25_Fixed_Popup {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
