4bd8c778e6c8a00d3e4f8c0e796ba0520e8f86a8
Fullstack selenium <dangtuananh94.it@gmail.com>
2024-05-08 00:44:26 +0700

    Test git

---
+++ selenium/javaTester/javaOop/javaAccModiSecond/Fish2.java
@@ -0,0 +1,8 @@
+package javaTester.javaOop.javaAccModiSecond;
+
+import javaTester.javaOop.javaAccModiFirst.Dog;
+
+public class Fish2 {
+   // Cat cat = new Cat(); loi vi class Cat la default class va khac package
+    Dog dog = new Dog();
+}
