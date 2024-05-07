d408884ef7d257c93b6ec5c196eb80b48371db11
Fullstack selenium <dangtuananh94.it@gmail.com>
2024-05-08 00:53:11 +0700

    delete Fish2

--- selenium/javaTester/javaOop/javaAccModiSecond/Fish2.java
+++
@@ -1,8 +0,0 @@
-package javaTester.javaOop.javaAccModiSecond;
-
-import javaTester.javaOop.javaAccModiFirst.Dog;
-
-public class Fish2 {
-   // Cat cat = new Cat(); loi vi class Cat la default class va khac package
-    Dog dog = new Dog();
-}
